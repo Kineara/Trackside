957b685e15abc73c0726f50f7b3ce0676cd620ed
Lauren Hoock <laurenh8916@protonmail.com>
2022-07-04 04:34:35 -0600

    deploy updates

--- src/routes/Drivers.js
+++ src/routes/Drivers.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container, Accordion } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
 import DriversFilter from "../components/DriversFilter";
